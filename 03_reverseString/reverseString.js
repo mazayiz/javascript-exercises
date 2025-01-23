4abfa34f29f136a575df063717c05ed4d627f7aa
Melvin Ramos <melvinr0114@gmail.com>
2025-01-23 15:09:56 -0800

    Finish 03 reverseString js exercise and pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversed = "";
+    for (let i = word.length; i>0; i--) {
+        let letter = word.slice(i-1,i);
+        reversed += letter;
+    }
+    return reversed;
 };
+
+// Test 1-4
+console.log(reverseString('hello'));
+console.log(reverseString('hello there'));
+console.log(reverseString('123! abc! Hello, Odinite.'));
+console.log(reverseString(''));
 
 // Do not edit below this line
 module.exports = reverseString;
