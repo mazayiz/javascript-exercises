15adf431c3dc86e2c831980af7ebc6674863eb72
Melvin Ramos <melvinr0114@gmail.com>
2025-01-27 16:30:11 -0800

    Finish 07_tempConversion JS and pass tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  // x °F ≘ (x − 32) × ⁠5/9⁠ °C 
+  // Condensed version
+  // return final = Number(((tempF - 32) * 5/9).toFixed(1))
+  let convert = (tempF - 32) * 5/9;
+  let oneDecimal = convert.toFixed(1);
+  let stringToNum = Number(oneDecimal);
+  return stringToNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  // x °C ≘ (x × ⁠9/5⁠ + 32) °F 
+  let convert = tempC * 9/5 + 32;
+  let oneDecimal = convert.toFixed(1);
+  let stringToNum = Number(oneDecimal);
+  return stringToNum;
 };
 
 // Do not edit below this line
